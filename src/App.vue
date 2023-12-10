465342843018e15e23dbaf5a2aed334869fc2c79
xudongLi <david188101279601@gmail.com>
2023-12-10 21:06:30 +0800

    updat

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,6 @@ export default {
     } catch (error) {
       console.error('Error in mounted hook:', error);
       // 在这里处理错误或设置备用逻辑
-      // 例如，如果 hydrated 方法失败，你可以选择设置 hydrated 为 true 或进行其他处理
       this.hydrated = true; // 可以根据实际情况调整
     }
   },
