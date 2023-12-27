5f1e6e385e7e75b54d0eca4962cf64409284a0b4
EC2 Default User <ec2-user@ip-172-31-56-80.ec2.internal>
2023-12-27 02:49:45 +0000

    Fix the anomaly in user creation (ユーザー作成時の問題異常を修復する)

--- src/components/FAQForm.vue
+++ src/components/FAQForm.vue
@@ -2,13 +2,13 @@
   <div data-v-13c3bc48="" data-v-688a1315="" class="sai-modal">
     <div data-v-13c3bc48="" class="inner">
       <div data-v-3fc1e8b2="" data-v-688a1315="" class="sai-enquete">
-        <form data-v-3fc1e8b2="" class="content input">
+        <form data-v-3fc1e8b2="" class="content input" @submit.prevent="submitForm">
           <div data-v-3fc1e8b2="" class="heading">
             <div data-v-3fc1e8b2="" class="title">
               <div data-v-3fc1e8b2="">ご質問を入力してください：</div>
               <div data-v-3fc1e8b2=""><span style="color: #ff0000">※個人情報を含む情報は返信できませんので、ご了承ください。</span></div>
             </div>
-            <div data-v-3fc1e8b2="" class="close" @click="closeForm"> <!-- Attach click event handler -->
+            <div data-v-3fc1e8b2="" class="close" @click="closeForm">
               <a data-v-3fc1e8b2="" class="icon">
                 <div data-v-3fc1e8b2="" class="img"></div>
               </a>
@@ -16,11 +16,11 @@
           </div>
           <div data-v-3fc1e8b2="" class="input">
             <label for="title">タイトル:</label>
-            <input data-v-3fc1e8b2="" type="text" id="title" class="field" placeholder="">
+            <input data-v-3fc1e8b2="" type="text" id="title" class="field" v-model="title" placeholder="">
           </div>
           <div data-v-3fc1e8b2="" class="input">
             <label for="category">カテゴリ:</label>
-            <select data-v-3fc1e8b2="" id="category" class="field">
+            <select data-v-3fc1e8b2="" id="category" class="field" v-model="category">
               <option value="分类1">カテゴリ1</option>
               <option value="分类2">カテゴリ2</option>
               <option value="分类3">カテゴリ3</option>
@@ -28,14 +28,14 @@
           </div>
           <div data-v-3fc1e8b2="" class="input">
             <label for="tags">タグ:</label>
-            <input data-v-3fc1e8b2="" type="text" id="tags" class="field" placeholder="">
+            <input data-v-3fc1e8b2="" type="text" id="tags" class="field" v-model="tags" placeholder="">
           </div>
           <div data-v-3fc1e8b2="" class="input">
             <label for="content">内容:</label>
-            <textarea data-v-3fc1e8b2="" type="text" id="content" rows="8" class="field" placeholder=""></textarea>
+            <textarea data-v-3fc1e8b2="" type="text" id="content" rows="8" class="field" v-model="content" placeholder=""></textarea>
           </div>
           <div data-v-3fc1e8b2="" class="submit">
-            <a data-v-3fc1e8b2="" class="send">問題の提出</a>
+            <button type="submit" data-v-3fc1e8b2="" class="send">問題の提出</button>
           </div>
         </form>
       </div>
@@ -45,14 +45,58 @@
 
 <script>
 import '../assets/FAQstyle.css';
+import CreateFAQ from "../mutations/CreateFAQ";
 export default {
+  data() {
+    return {
+      title: '',
+      category: '',
+      tags: '',
+      content: '',
+    };
+  },
   methods: {
     closeForm() {
       // Trigger the close event to notify the parent component to close the form
       this.$emit("close");
     },
+
+    generateRandomId() {
+      return Math.floor(Math.random() * 9000000000) + 1000000000; // 生成一个十位数的随机数
+    },
+
+    getCurrentISOTime() {
+      return new Date().toISOString(); // 获取当前时间的 ISO 格式字符串
+    },
+
+    async submitForm() {
+      try {
+        const input = {
+          id: this.generateRandomId(),
+          title: this.title,
+          category: this.category,
+          tag: this.tags,
+          detail: this.content,
+          createtime: this.getCurrentISOTime(),
+        };
+        console.log(input);
+
+        await this.$apollo.mutate({
+          mutation: CreateFAQ,
+          variables: {
+            createnrifqainput: input,
+          },
+        });
+
+        this.closeForm();
+      } catch (error) {
+        console.error('Error submitting FAQ:', error);
+        // 处理错误
+      }
+    },
   },
 };
+</script>
 </script>
 
 <style scoped>
